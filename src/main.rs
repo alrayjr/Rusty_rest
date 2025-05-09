02a6f79d88eb9bd4036c7a7a328e3d77faa9a9eb
alrayjr <adrianrayjr@gmail.com>
2025-05-09 12:07:06 -0400

    logger and greet added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::sync::Mutex;
 
 // This struct represents state
@@ -13,11 +13,24 @@ async fn index(data: web::Data<AppStateWithCounter>) -> String {
     format!("Request number: {counter}") // <- response with count
 }
 
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there cool guy")
 }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    unsafe{
+        if std::env::var_os("RUST_LOG").is_none() {
+            std::env::set_var("RUST_LOG", "actix_web=info");
+        }
+    }
+    dotenv::dotenv().ok();
+    env_logger::init();
+
     // Note: web::Data created _outside_ HttpServer::new closure
     let counter = web::Data::new(AppStateWithCounter {
         counter: Mutex::new(0),
@@ -26,9 +39,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         // move counter into the closure
         App::new()
+            .wrap(actix_web::middleware::Logger::default())
+            .service(greet)
             .app_data(counter.clone()) // <- register the created data
             .route("/", web::get().to(index))
-            .route("/hello",web::get().to(manual_hello))
+            .route("/hi",web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
